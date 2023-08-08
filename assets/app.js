d39a53a0dce262d62b45251bee4c5b5f991ea0dd
huzaifamk <malikhuzaifa99@gmail.com>
2023-08-09 01:54:30 +0500

    Made the Vue and WASM connection success

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,73 @@
 new Vue({
   el: "#app",
   data: {
-    // Data properties (for future use)
+    showLoginForm: false,
+    showRegisterForm: false,
+    showInitialButtons: true,
+    showImage: false,
+    loginUsername: "",
+    loginPassword: "",
+    selectedChoice: "1",
+    registerUsername: "",
+    registerPassword: "",
+    repeatPassword: "",
+    imageSrc: "", // Set the image source here
+    loginMessage: "",
+    repeatPasswordError: "",
   },
   methods: {
-    showLoginForm() {
-      this.showLoginForm = true;
-      this.showInitialButtons = false;
-      this.showRegisterForm = false;
+    logUser() {
+      const username = this.loginUsername;
+      const password = this.loginPassword;
+      const choice = this.selectedChoice;
+      // Call your Go function to login the user
+      const loginResult = loginUser(username, password, choice);
+      if (loginResult === "success") {
+        this.loginMessage = "Login successful";
+      } else {
+        this.loginMessage = "Login failed";
+      }
     },
-    showRegisterForm() {
-      this.showRegisterForm = true;
-      this.showInitialButtons = false;
-      this.showLoginForm = false;
+    regUser() {
+      const username = this.registerUsername;
+      const password = this.registerPassword;
+      const repeatPassword = this.repeatPassword;
+      const repeatPasswordError = document.getElementById(
+        "repeatPasswordError"
+      );
+
+      if (password !== repeatPassword) {
+        this.repeatPasswordError = "Passwords do not match";
+        return;
+      }
+
+      // Call your Go function to register the user
+      const registerResult = registerUser(username, password);
+      if (registerResult === "success") {
+        this.repeatPasswordError = "User registered";
+      } else {
+        this.repeatPasswordError = "User already exists";
+      }
     },
     goBack() {
       this.showLoginForm = false;
       this.showRegisterForm = false;
       this.showInitialButtons = true;
-      // Reset other form fields and messages
+      this.showImage = false;
+      this.loginMessage = "";
+      this.repeatPasswordError = "";
     },
-    // Other methods (for future use)
+    showLoginFormSection() {
+      this.showLoginForm = true;
+      this.showRegisterForm = false;
+      this.showInitialButtons = false;
+      this.showImage = false;
+    },
+    showRegisterFormSection() {
+      this.showLoginForm = false;
+      this.showRegisterForm = true;
+      this.showInitialButtons = false;
+      this.showImage = false;
+    },
   },
 });
